fa8e73efd75d1151875dfdf75c5cbe4aee1e061b
Debanjan <debanjanghosal10@gmail.com>
2024-04-29 09:54:39 +0530

    Added server side functionality for DP upload

--- server/routes/AuthRouter.js
+++ server/routes/AuthRouter.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const { registerUser, loginUser } = require('../controllers/Auth')
+const { profilePictureUpload } = require('../controllers/Upload')
 const AuthRouter = express.Router()
 
 AuthRouter.post("/register",registerUser)
 
 AuthRouter.post("/login",loginUser)
+
+AuthRouter.post("/register/upload",profilePictureUpload)
 
 module.exports = AuthRouter
--- server/routes/UserRouter.js
+++ server/routes/UserRouter.js
@@ -4,6 +4,7 @@ const UserRouter = express.Router()
 
 UserRouter.get("/:id",getUserDetails)
 
+
 module.exports = UserRouter
 
 // /api/v1/profile/<id>
