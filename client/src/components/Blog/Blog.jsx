dcc7ef714e6530ac2f6877375fc37ac72039fd69
Sama-004 <70210929+Sama-004@users.noreply.github.com>
2024-05-02 15:16:05 +0530

    fixed merge bugs

--- client/src/components/Blog/Blog.jsx
+++ client/src/components/Blog/Blog.jsx
@@ -1,10 +1,10 @@
 /* eslint-disable react/prop-types */
-import LazyLoad from 'react-lazyload'
-import { useNavigate } from 'react-router-dom'
+import LazyLoad from "react-lazyload";
+import { useNavigate } from "react-router-dom";
 import { FaRegEdit } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
-import axios from "axios"
-import { useEffect, useState } from 'react';
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -18,32 +18,35 @@ import {
   DrawerTrigger,
 } from "@/components/ui/drawer";
 
-const Blog = ({ id, title, intro, content, img, tags, isLoggedIn, author, createdAt }) => {
-
-  const [username, setUsername] = useState('')
-  const navigateTo = useNavigate()
-
-
-
+const Blog = ({
+  id,
+  title,
+  intro,
+  content,
+  img,
+  tags,
+  isLoggedIn,
+  author,
+  createdAt,
+}) => {
+  const [username, setUsername] = useState("");
+  const navigateTo = useNavigate();
 
   useEffect(() => {
-    axios.get(`/api/v1/profile/${author}`).then(res => {
-      setUsername(res.data.user.username)
-    }).catch(err =>
-      console.log(err))
-  }, [])
+    axios
+      .get(`/api/v1/profile/${author}`)
+      .then((res) => {
+        setUsername(res.data.user.username);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
-  const originalDate = new Date(createdAt)
+  const originalDate = new Date(createdAt);
   const options = { year: "numeric", month: "long", day: "numeric" };
   const formattedDate = originalDate.toLocaleString("en-US", options);
 
-  // const [toEdit, setToEdit] = useState(false)
-
-  const token = localStorage.getItem("jwt")
-const Blog = ({ id, title, intro, content, img, tags, isLoggedIn }) => {
-  const [isDeleteDrawerOpen, setIsDeleteDrawerOpen] = useState(false);
-  const navigateTo = useNavigate();
   const token = localStorage.getItem("jwt");
+  const [isDeleteDrawerOpen, setIsDeleteDrawerOpen] = useState(false);
 
   const handleDeleteClick = () => {
     setIsDeleteDrawerOpen(true);
@@ -75,57 +78,143 @@ const Blog = ({ id, title, intro, content, img, tags, isLoggedIn }) => {
 
   const handleEditClick = () => {
     // setToEdit(true)
-    navigateTo("/profile/edit", { state: { id, title, intro, content, img, tags } })
+    navigateTo("/profile/edit", {
+      state: { id, title, intro, content, img, tags },
+    });
+  };
 
-  }
-
-
-  const handleDeleteClick = () => {
-    axios.delete(`/api/v1/posts/${id}`, { headers }).then(res => {
-      console.log(res);
-      navigateTo("/profile", { state: { refetchPosts: true } })
-    }).catch(err => console.log(err));
-  }
-
-
+  // const handleDeleteClick = () => {
+  //   axios.delete(`/api/v1/posts/${id}`, { headers }).then(res => {
+  //     console.log(res);
+  //     navigateTo("/profile", { state: { refetchPosts: true } })
+  //   }).catch(err => console.log(err));
+  // };
 
   const handleImageClick = () => {
     console.log(username, formattedDate);
-    navigateTo(`/blog/${id}`, { state: { username, formattedDate } })
-  }
+    navigateTo(`/blog/${id}`, { state: { username, formattedDate } });
+  };
 
+  //   return (
+  //     <>
+  //       <div className="flex items-center gap-5 justify-center mx-4 my-10">
+  //         <LazyLoad>
+  //           <div className="flex flex-col items-center justify-center">
+  //             <img
+  //               src={img}
+  //               alt=""
+  //               className="h-64 object-cover rounded-lg hover:cursor-pointer hover:scale-105 transition my-4"
+  //               onClick={handleImageClick}
+  //             />
+  //             {isLoggedIn && (
+  //               <div key={id} className="flex gap-4 my-1">
+  //                 <FaRegEdit
+  //                   onClick={handleEditClick}
+  //                   className="text-3xl hover:text-green-600 hover:cursor-pointer"
+  //                 />
+  //                 <MdDelete
+  //                   onClick={handleDeleteClick}
+  //                   className="text-3xl hover:text-red-500 hover:cursor-pointer"
+  //                 />
+  //               </div>
+  //             )}
+  //           </div>
+  //         </LazyLoad>
 
+  //         <div className="flex flex-col w-1/2 ">
+  //           <p className="text-slate-800 text-center my-2">
+  //             {" "}
+  //             {username} • {formattedDate}
+  //           </p>
+  //           <h1 className="text-2xl font-semibold text-left">{title}</h1>
 
+  //           <div className="flex  gap-2 flex-wrap justify-center">
+  //             {tags &&
+  //               tags.map((tag, index) => (
+  //                 <p
+  //                   key={index}
+  //                   className="text-sm font-semibold text-center my-1 min-w-20 bg-sky-400 text-slate-800 p-2 rounded-lg">
+  //                   {tag}
+  //                 </p>
+  //               ))}
+  //           </div>
+  //           <p className="text-lg my-4 text-left">{intro}</p>
+  //         </div>
+  //         <h1 className="text-2xl font-semibold text-center">{title}</h1>
+  //       </div>
+  //       <hr className='bg-red-500' />
+  //              </div >
+  //       <hr className='bg-slate-700 h-2 shadow-md w-1/2' />
+  //       <Drawer open={isDeleteDrawerOpen} onClose={handleCloseDeleteDrawer}>
+  //         <DrawerContent>
+  //           <div className="mx-auto w-full max-w-sm">
+  //             <DrawerHeader>
+  //               <DrawerTitle>Delete Blog Post</DrawerTitle>
+  //               <DrawerDescription>
+  //                 {"You can't recover this post once you delete it"}
+  //               </DrawerDescription>
+  //             </DrawerHeader>
+  //             <DrawerFooter>
+  //               <Button onClick={handleConfirmDelete} className="bg-red-500">
+  //                 Delete
+  //               </Button>
+  //               <DrawerClose asChild>
+  //                 <Button onClick={handleCloseDeleteDrawer} variant="outline">
+  //                   Cancel
+  //                 </Button>
+  //               </DrawerClose>
+  //             </DrawerFooter>
+  //           </div>
+  //         </DrawerContent>
+  //       </Drawer>
+  //     </>
+  //   );
+  // };
   return (
     <>
-      <div className='flex items-center gap-5 justify-center mx-4 my-10'>
-
+      <div className="flex items-center gap-5 justify-center mx-4 my-10">
         <LazyLoad>
-          <div className='flex flex-col items-center justify-center'>
-            <img src={img} alt="" className='h-64 object-cover rounded-lg hover:cursor-pointer hover:scale-105 transition my-4' onClick={handleImageClick} />
-            {isLoggedIn &&
+          <div className="flex flex-col items-center justify-center">
+            <img
+              src={img}
+              alt=""
+              className="h-64 object-cover rounded-lg hover:cursor-pointer hover:scale-105 transition my-4"
+              onClick={handleImageClick}
+            />
+            {isLoggedIn && (
               <div key={id} className="flex gap-4 my-1">
-                <FaRegEdit onClick={handleEditClick} className='text-3xl hover:text-green-600 hover:cursor-pointer' />
-                <MdDelete onClick={handleDeleteClick} className='text-3xl hover:text-red-500 hover:cursor-pointer' />
+                <FaRegEdit
+                  onClick={handleEditClick}
+                  className="text-3xl hover:text-green-600 hover:cursor-pointer"
+                />
+                <MdDelete
+                  onClick={handleDeleteClick}
+                  className="text-3xl hover:text-red-500 hover:cursor-pointer"
+                />
               </div>
-            }
+            )}
           </div>
         </LazyLoad>
 
         <div className="flex flex-col w-1/2 ">
-          <p className='text-slate-800 text-center my-2'> {username} • {formattedDate}</p>
-          <h1 className='text-2xl font-semibold text-left'>{title}</h1>
+          <p className="text-slate-800 text-center my-2">
+            {" "}
+            {username} • {formattedDate}
+          </p>
+          <h1 className="text-2xl font-semibold text-left">{title}</h1>
 
-
-          <div className="flex  gap-2 flex-wrap justify-center" >
-            {tags && tags.map((tag, index) => <p key={index} className='text-sm font-semibold text-center my-1 min-w-20 bg-sky-400 text-slate-800 p-2 rounded-lg'>{tag}</p>)}
+          <div className="flex  gap-2 flex-wrap justify-center">
+            {tags &&
+              tags.map((tag, index) => (
+                <p
+                  key={index}
+                  className="text-sm font-semibold text-center my-1 min-w-20 bg-sky-400 text-slate-800 p-2 rounded-lg">
+                  {tag}
+                </p>
+              ))}
           </div>
-          <p className='text-lg my-4 text-left'>{intro}</p>
-
+          <p className="text-lg my-4 text-left">{intro}</p>
         </div>
-        {/* <p className='my-4 text-blue-600'>{tag}</p> */}
-        <h1 className="text-2xl font-semibold text-center">{title}</h1>
-        <p className="text-lg text-center my-4">{intro}</p>
       </div>
       <Drawer open={isDeleteDrawerOpen} onClose={handleCloseDeleteDrawer}>
         <DrawerContent>
@@ -133,7 +222,7 @@ const Blog = ({ id, title, intro, content, img, tags, isLoggedIn }) => {
             <DrawerHeader>
               <DrawerTitle>Delete Blog Post</DrawerTitle>
               <DrawerDescription>
-                You can't recover this post once you delete it
+                {"You can't recover this post once you delete it"}
               </DrawerDescription>
             </DrawerHeader>
             <DrawerFooter>
@@ -154,13 +243,3 @@ const Blog = ({ id, title, intro, content, img, tags, isLoggedIn }) => {
 };
 
 export default Blog;
-        <hr className='bg-red-500' />
-      </div >
-      <hr className='bg-slate-700 h-2 shadow-md w-1/2' />
-
-    </>
-
-  )
-}
-
-export default Blog
--- client/src/components/Blog/Blogs.jsx
+++ client/src/components/Blog/Blogs.jsx
@@ -1,34 +1,43 @@
 import { useEffect, useState } from "react";
 import Blog from "./Blog";
-import axios from 'axios'
-import NoPostsImage from "../../assets/No-posts.png"
+import axios from "axios";
+import NoPostsImage from "../../assets/No-posts.png";
 
 const Blogs = () => {
-
-  const [posts, setPosts] = useState([])
+  const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    axios.get("/api/v1/posts").then(res => {
-      console.log(res.data.posts);
-      setPosts([...res.data.posts])
-    }).catch(err => {
-      console.log(err);
-    })
-  }, [])
+    axios
+      .get("/api/v1/posts")
+      .then((res) => {
+        console.log(res.data.posts);
+        setPosts([...res.data.posts]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   if (posts.length === 0) {
-    return <div className="flex flex-col items-center justify-center h-screen">
-
-      <h1 className='text-center text-6xl my-4 w-1/2'>Sorry, there are currently no blog posts available.</h1>
-      <img src={NoPostsImage} alt="" className="h-80" />
-      <h1 className='text-center text-4xl my-4 w-1/2'> Check back later for updates!</h1>
-    </div>
+    return (
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-center text-6xl my-4 w-1/2">
+          Sorry, there are currently no blog posts available.
+        </h1>
+        <img src={NoPostsImage} alt="" className="h-80" />
+        <h1 className="text-center text-4xl my-4 w-1/2">
+          {" "}
+          Check back later for updates!
+        </h1>
+      </div>
+    );
   }
 
   return (
-
-    <div className='text-slate-700 w-full mx-auto my-10' id='blogs'>
-      <h1 className=' text-6xl advent-pro-medium font-bold text-center my-5'>Recent Blogs</h1>
+    <div className="text-slate-700 w-full mx-auto my-10" id="blogs">
+      <h1 className=" text-6xl advent-pro-medium font-bold text-center my-5">
+        Recent Blogs
+      </h1>
       <div className="blogs flex items-center flex-col">
         {posts.map((post) => (
           <Blog
@@ -44,7 +53,6 @@ const Blogs = () => {
         ))}
       </div>
     </div>
-
   );
 };
 
