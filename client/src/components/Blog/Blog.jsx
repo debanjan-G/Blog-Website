ab951d535ef497cf1291fea1ad804c40949165e9
Sama-004 <70210929+Sama-004@users.noreply.github.com>
2024-05-02 15:20:01 +0530

    fixed merge bugs

--- client/src/components/Blog/Blog.jsx
+++ client/src/components/Blog/Blog.jsx
@@ -57,6 +57,11 @@ const Blog = ({
   };
 
   const handleConfirmDelete = () => {
+    //   axios.delete(`/api/v1/posts/${id}`, { headers }).then(res => {
+    //     console.log(res);
+    //     navigateTo("/profile", { state: { refetchPosts: true } })
+    //   }).catch(err => console.log(err));
+    // };
     axios
       .delete(`/api/v1/posts/${id}`, { headers })
       .then((res) => {
@@ -95,81 +100,6 @@ const Blog = ({
     navigateTo(`/blog/${id}`, { state: { username, formattedDate } });
   };
 
-  //   return (
-  //     <>
-  //       <div className="flex items-center gap-5 justify-center mx-4 my-10">
-  //         <LazyLoad>
-  //           <div className="flex flex-col items-center justify-center">
-  //             <img
-  //               src={img}
-  //               alt=""
-  //               className="h-64 object-cover rounded-lg hover:cursor-pointer hover:scale-105 transition my-4"
-  //               onClick={handleImageClick}
-  //             />
-  //             {isLoggedIn && (
-  //               <div key={id} className="flex gap-4 my-1">
-  //                 <FaRegEdit
-  //                   onClick={handleEditClick}
-  //                   className="text-3xl hover:text-green-600 hover:cursor-pointer"
-  //                 />
-  //                 <MdDelete
-  //                   onClick={handleDeleteClick}
-  //                   className="text-3xl hover:text-red-500 hover:cursor-pointer"
-  //                 />
-  //               </div>
-  //             )}
-  //           </div>
-  //         </LazyLoad>
-
-  //         <div className="flex flex-col w-1/2 ">
-  //           <p className="text-slate-800 text-center my-2">
-  //             {" "}
-  //             {username} • {formattedDate}
-  //           </p>
-  //           <h1 className="text-2xl font-semibold text-left">{title}</h1>
-
-  //           <div className="flex  gap-2 flex-wrap justify-center">
-  //             {tags &&
-  //               tags.map((tag, index) => (
-  //                 <p
-  //                   key={index}
-  //                   className="text-sm font-semibold text-center my-1 min-w-20 bg-sky-400 text-slate-800 p-2 rounded-lg">
-  //                   {tag}
-  //                 </p>
-  //               ))}
-  //           </div>
-  //           <p className="text-lg my-4 text-left">{intro}</p>
-  //         </div>
-  //         <h1 className="text-2xl font-semibold text-center">{title}</h1>
-  //       </div>
-  //       <hr className='bg-red-500' />
-  //              </div >
-  //       <hr className='bg-slate-700 h-2 shadow-md w-1/2' />
-  //       <Drawer open={isDeleteDrawerOpen} onClose={handleCloseDeleteDrawer}>
-  //         <DrawerContent>
-  //           <div className="mx-auto w-full max-w-sm">
-  //             <DrawerHeader>
-  //               <DrawerTitle>Delete Blog Post</DrawerTitle>
-  //               <DrawerDescription>
-  //                 {"You can't recover this post once you delete it"}
-  //               </DrawerDescription>
-  //             </DrawerHeader>
-  //             <DrawerFooter>
-  //               <Button onClick={handleConfirmDelete} className="bg-red-500">
-  //                 Delete
-  //               </Button>
-  //               <DrawerClose asChild>
-  //                 <Button onClick={handleCloseDeleteDrawer} variant="outline">
-  //                   Cancel
-  //                 </Button>
-  //               </DrawerClose>
-  //             </DrawerFooter>
-  //           </div>
-  //         </DrawerContent>
-  //       </Drawer>
-  //     </>
-  //   );
-  // };
   return (
     <>
       <div className="flex items-center gap-5 justify-center mx-4 my-10">
